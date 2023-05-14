b0e28aef8677865265b06a451847896b3ad2ce8e
myichikawa <myickw24@gmail.com>
2023-05-14 20:49:17 +0900

    Lesson35 日報修正

--- src/main/java/com/techacademy/controller/TopController.java
+++ src/main/java/com/techacademy/controller/TopController.java
@@ -1,11 +1,13 @@
 package com.techacademy.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.techacademy.entity.Employee;
 import com.techacademy.service.ReportService;
+import com.techacademy.service.UserDetail;
 
 @Controller
 public class TopController {
@@ -17,8 +19,8 @@ public class TopController {
     }
 
     @GetMapping("/")
-    public String getTop(Employee employee, Model model) {
-        model.addAttribute("employeereport",service.getEmployeeReport(employee));
+    public String getTop(Employee employee, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+        model.addAttribute("employeereport",service.getEmployeeReport(userDetail.getEmployee()));
         model.addAttribute("reportcount", service.index());
 
         return "top";
